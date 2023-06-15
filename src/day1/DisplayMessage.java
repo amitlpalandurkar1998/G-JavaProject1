0ead3acc3b9b2db1173c078af5c7743b81ee983d
Amit <amitlpalandurkar1998@gmail.com>
2023-06-15 14:46:43 +0530

    Changes-fixes

---
+++ src/day1/DisplayMessage.java
@@ -0,0 +1,9 @@
+package day1;
+
+public class DisplayMessage {
+    public static void main(String[] args) {
+//        Write a program to display any message:
+
+        System.out.printf("Hello, My Name is Amit Palandurkar.");
+    }
+}
